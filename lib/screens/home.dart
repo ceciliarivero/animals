6819dac490919dd06fcd7697bbeaed9034436dba
C Rivero <crivero@macbookpro-8.local>
2023-03-13 19:29:42 +0000

    Add header image and main container to Home screen.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,30 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              "https://img.freepik.com/free-vector/watercolor-wildlife-background_52683-84955.jpg",
+              fit: BoxFit.fitWidth,
+              semanticLabel: 'Animals illustration',
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Column(
+                children: const [
+                  Placeholder(),
+                  Placeholder(),
+                  Placeholder(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
