1dc9ec1cbe0713808acd294d18c4b3ff50263520
C Rivero <crivero@macbookpro-8.local>
2023-03-14 18:50:59 +0000

    Add data file and Animal model.

---
+++ lib/models/animal.dart
@@ -0,0 +1,13 @@
+class Animal {
+  final String id;
+  final String name;
+  final String imagePath;
+  final String description;
+
+  Animal({
+    required this.id,
+    required this.name,
+    required this.imagePath,
+    required this.description,
+  });
+}
